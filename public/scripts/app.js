7e28181b47d87a8894051e6031c68a1bfd471d49
MaluPalu <malu.peralta808@gmail.com>
2017-08-31 12:30:32 -0700

    Sprint 1.2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+
 console.log('Sanity Checl');
 /* CLIENT-SIDE JS
  *
@@ -34,20 +35,75 @@ sampleAlbums.push({
              genres: [ 'piano' ]
            });
 /* end of hard-coded data */
+$.ajax({
+    method: 'GET',
+    url: '/api/albums',
+    success: handleSuccess,
+    error: handleError
+  });
 
+function handleSuccess (albums) {
+    albums.forEach(function(album) {
+      renderAlbum(album);
+    });
+};
 
-
+function handleError(err){
+  console.log('There has been an error: ', err);
+}
 
 $(document).ready(function() {
   console.log('app.js loaded!');
+  renderAlbum(sampleAlbums[0]);
 });
-
-
-
-
-
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
   console.log('rendering album:', album);
+  for (let i = 0; i < sampleAlbums.length; i++) {
+      var myAlbums = (`<div class="row album">
 
-}
+        <div class="col-md-10 col-md-offset-1">
+          <div class="panel panel-default">
+            <div class="panel-body">
+
+
+            <!-- begin album internal row -->
+              <div class='row'>
+                <div class="col-md-3 col-xs-12 thumbnail album-art">
+                  <img src="/images/800x800.png" alt="album image">
+                </div>
+
+                <div class="col-md-9 col-xs-12">
+                  <ul class="list-group">
+                    <li class="list-group-item">
+                      <h4 class='inline-header'>Album Name:</h4>
+                      <span class='album-name'>${sampleAlbums[i].name}</span>
+                    </li>
+
+                    <li class="list-group-item">
+                      <h4 class='inline-header'>Artist Name:</h4>
+                      <span class='artist-name'>${sampleAlbums[i].artistName}</span>
+                    </li>
+
+                    <li class="list-group-item">
+                      <h4 class='inline-header'>Released date:</h4>
+                      <span class='album-releaseDate'>${sampleAlbums[i].releaseDate}</span>
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+              <!-- end of album internal row -->
+
+              <div class='panel-footer'>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      `)
+      $('#albums').append(myAlbums);
+  };
+
+};
